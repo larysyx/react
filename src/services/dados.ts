8d72e34bb93b7c8892489a66e92d50ac54e1db8b
larysyx <102804266+larysyx@users.noreply.github.com>
2022-07-15 16:10:17 -0300

    Add files via upload

---
+++ src/services/dados.ts
@@ -0,0 +1,137 @@
+const dados = [
+    {
+        id: 1,
+        nome: "Lázaro",
+        data: "27/04/2022 10:08:00",
+        titulo: "Blusa",
+        mensagem: "Tenho uma blusa para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }]
+    },
+    {
+        id: 2,
+        nome: "Mirabel",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 3,
+        nome: "Isabela",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 4,
+        nome: "Bruno",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 4,
+        nome: "Luisa",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 6,
+        nome: "Pepa",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 7,
+        nome: "Dolores",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+    {
+        id: 8,
+        nome: "Abuela",
+        data: "28/04/2022 10:08:00",
+        titulo: "Calça",
+        mensagem: "Tenho uma calça para doar, alguem interessa?",
+        topico: [{
+            id: 1,
+            item: "Reutilizar"
+        }, {
+            id: 2,
+            item: "Reciclar"
+        }, {
+            id: 3,
+            item: "Doar"
+        }]
+    },
+]
+
+export default dados
